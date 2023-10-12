0c1d7d0af29b4645a64f7f122b40d0478f5ec5bc
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-10-12 19:16:38 +0500

    Task6

---
+++ Tyuiu.LyaginVA.Sprint2.Task6.V2.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LyaginVA.Sprint2.Task6.V2.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LyaginVA.Sprint2.Task6.V2.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cfa7306c-ca51-4ed9-8f52-1390a41b8a12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LyaginVA.Sprint2.Task6.V2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LyaginVA.Sprint2.Task6.V2.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint2.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Лягин В. Л. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Получение результата из switch                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Лягин Владимир Александрович | АСОиУБ-23-2                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет   требуемое значение и возвращает результат. *");
+            Console.WriteLine("* По данному месяцу, определите в какую пору года попадает этот месяц     *");
+            Console.WriteLine("* (Зима, Лето, Весна, Осень).                                             *");
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите номер месяца: ");
+            int numMonthSeason = Convert.ToInt32(Console.ReadLine());
+
+            string res;
+
+            if ((numMonthSeason < 1) || (numMonthSeason > 12))
+            {
+                res = "Введенно неверное значение!";
+            }
+            else
+            {
+                res = "Это пора года: " + ds.FindMonthSeason(numMonthSeason);
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
