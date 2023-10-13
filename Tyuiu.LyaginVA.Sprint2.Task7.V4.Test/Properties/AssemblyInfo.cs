8d75737f3459d0044da85b065a2372aa25907a6a
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-10-13 15:42:12 +0500

    Task7

---
+++ Tyuiu.LyaginVA.Sprint2.Task7.V4.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LyaginVA.Sprint2.Task7.V4.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LyaginVA.Sprint2.Task7.V4.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("78f5298c-8625-4104-a81b-8392eeaf3f29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
